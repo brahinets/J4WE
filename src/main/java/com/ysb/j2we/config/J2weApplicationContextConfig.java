97c87dbb44485ff2d7956ff987c48678a6dabb5b
ybrahinets <y.brahinets@spd-ukraine.com>
2016-11-03 13:32:41 +0200

    Move persistence config parameters to properties file

--- src/main/java/com/ysb/j2we/config/J2weApplicationContextConfig.java
+++ src/main/java/com/ysb/j2we/config/J2weApplicationContextConfig.java
@@ -1,8 +1,10 @@
 package com.ysb.j2we.config;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -29,5 +31,15 @@ public class J2weApplicationContextConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/res/**").addResourceLocations("/res/");
+    }
+
+    @Bean
+    public PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer() {
+        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+        
+        ppc.setLocations(new ClassPathResource("application.properties"));
+        ppc.setIgnoreUnresolvablePlaceholders(true);
+        
+        return ppc;
     }
 }
--- src/main/java/com/ysb/j2we/config/J2wePersistenceConfig.java
+++ src/main/java/com/ysb/j2we/config/J2wePersistenceConfig.java
@@ -1,6 +1,7 @@
 package com.ysb.j2we.config;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,20 +16,41 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class J2wePersistenceConfig {
 
+    @Value("${jdbc.driverClassName}")
+    private String jdbcDriverClassName;
+
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+
+    @Value("${jdbc.username}")
+    private String jdbcUsername;
+
+    @Value("${jdbc.password}")
+    private String jdbcPassword;
+
+    @Value("${hibernate.dialect}")
+    private String hibernateDialect;
+
+    @Value("${hibernate.show_sql}")
+    private boolean hibernateShowSql;
+
+    @Value("${hibernate.format_sql}")
+    private boolean hibernateFormatSql;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource ds = new DriverManagerDataSource();
 
-        ds.setDriverClassName("oracle.jdbc.OracleDriver");
-        ds.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
-        ds.setUsername("test");
-        ds.setPassword("password");
+        ds.setDriverClassName(jdbcDriverClassName);
+        ds.setUrl(jdbcUrl);
+        ds.setUsername(jdbcUsername);
+        ds.setPassword(jdbcPassword);
 
         return ds;
     }
-    
+
     @Bean
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory){
+    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
         return new HibernateTransactionManager(sessionFactory);
     }
 
@@ -46,9 +68,9 @@ public class J2wePersistenceConfig {
     private Properties getHibernateProperties() {
         Properties properties = new Properties();
 
-        properties.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
-        properties.put("hibernate.show_sql", true);
-        properties.put("hibernate.format_sql", true);
+        properties.put("hibernate.dialect", hibernateDialect);
+        properties.put("hibernate.show_sql", hibernateShowSql);
+        properties.put("hibernate.format_sql", hibernateFormatSql);
 
         return properties;
     }
